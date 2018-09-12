316874c76e14e8f1ba84de916bb7128f36c4708d
Arthur Ruf Hosang da Costa <arthur.rhc@gmail.com>
2018-09-12 17:46:53 -0300

    Minor bug fixes

--- client/src/app/messages-view/messages-view.component.ts
+++ client/src/app/messages-view/messages-view.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import {Location} from '@angular/common';
+
 
 @Component({
   selector: 'app-messages-view',
@@ -7,9 +9,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MessagesViewComponent implements OnInit {
 
-  constructor() { }
+  constructor(private _location: Location) { }
 
   ngOnInit() {
+  }
+
+  goBack() {
+    this._location.back();
   }
 
 }
